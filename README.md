55d964f4f3bd34aa4fd2791cdf6c6dedab013f77
Francis Go <francis.go@gmail.com>
2017-06-12 23:09:17 +1000

    Word Blanks

--- README.md
+++ README.md
@@ -171,7 +171,7 @@ Front End Development challenges.
 - [x] Use Bracket Notation to Find the Nth Character in a String
 - [x] Use Bracket Notation to Find the Last Character in a String
 - [x] Use Bracket Notation to Find the Nth-to-Last Character in a String
-- [ ] Word Blanks
+- [x] Word Blanks
 - [ ] Store Multiple Values in one Variable using JavaScript Arrays
 - [ ] Nest one Array within Another Array
 - [ ] Access Array Data with Indexes
---
+++ front-end-development-challenges/basic-javascript/036-word-blanks.js
@@ -0,0 +1,17 @@
+/*!
+ * Word Blanks - Front End Development Certification
+ *
+ * Authored by Francis Go
+ * https://github.com/fgo/freecodecamp
+ */
+
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  var result = "";
+  // Your code below this line
+  result = myNoun + ' ' + myAdjective + ' ' + myVerb + ' ' + myAdverb;
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
