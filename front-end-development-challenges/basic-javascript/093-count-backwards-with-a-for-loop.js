167e87cab439e707ac002ff20d90711c2c91ef24
Francis Go <francis.go@gmail.com>
2017-06-17 00:49:53 +1000

    Count Backwards With a For Loop

---
+++ front-end-development-challenges/basic-javascript/093-count-backwards-with-a-for-loop.js
@@ -0,0 +1,21 @@
+/*!
+ * Count Backwards With a For Loop - Front End Development Certification
+ *
+ * Authored by Francis Go
+ * https://github.com/fgo/freecodecamp
+ */
+
+// Example
+var ourArray = [];
+
+for (var i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 9; i > 0; i -= 2) {
+  myArray.push(i);
+}
