d17d51acd394374c97bb9881d867b2ac6a55a17c
Francis Go <francis.go@gmail.com>
2017-06-18 23:42:39 +1000

    Update multi-line comment

--- front-end-development-challenges/basic-algorithm-scripting/003-factorialize-a-number.js
+++ front-end-development-challenges/basic-algorithm-scripting/003-factorialize-a-number.js
@@ -5,17 +5,17 @@
  * https://github.com/fgo/freecodecamp
  */
 
-/* For Loop
- * function factorialize(num) {
- *   if (num === 0 || num === 1) {
- *     return 1;
- *   }
- *
- *   for (var i = num - 1; i >= 1; i--) {
- *     num *= i;
- *   }
- *   return num;
- * }
+/* For Loop example
+function factorialize(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+
+  for (var i = num - 1; i >= 1; i--) {
+    num *= i;
+  }
+  return num;
+}
 */
 
 // Recursion
